353f782eeb8d7fdb0c3c35521407ee868272a8f2
vmlinuz719 <dlevine.levine@gmail.com>
2020-11-21 01:15:29 -0500

    added view code

--- view.c
+++ view.c
@@ -14,6 +14,11 @@
 #include <Xm/MainW.h>
 #include <Xm/CascadeB.h>
 
+typedef struct {
+	Widget topLevel, mainWindow, menuBar, quit, help;
+	XtAppContext context;
+} View;
+
 void quitCallback() {
 	printf("Exiting.\n");
 	exit(0);
@@ -23,10 +28,42 @@ void helpCallback() {
 	printf("Not implemented, sorry!\n");
 }
 
-Widget topLevel, mainWindow, menuBar, quit, help;
-
 #ifdef BUILD
 int main(int argc, char *argv[]) {
+	View *view = malloc(sizeof(view));
+	if (view == NULL) {
+		return EXIT_FAILURE;
+	}
+	
+	Arg arg[1];
+	
+	view->topLevel = XtVaAppInitialize(&(view->context), "Vx", NULL, 0,
+			&argc, argv, NULL, NULL);
+	
+	view->mainWindow = XtVaCreateManagedWidget("mainWindow",
+			xmMainWindowWidgetClass, view->topLevel,
+			NULL);
+	
+	view->menuBar = XmCreateMenuBar(view->mainWindow, "mainMenu", NULL, 0);
+	XtManageChild(view->menuBar);
+	
+	view->quit = XtCreateManagedWidget("Quit",
+			xmCascadeButtonWidgetClass, view->menuBar,
+			XmNmnemonic, 'Q',
+			NULL);
+	XtAddCallback(view->quit, XmNactivateCallback, quitCallback, NULL);
+	
+	view->help = XtCreateManagedWidget("Help",
+			xmCascadeButtonWidgetClass, view->menuBar,
+			XmNmnemonic, 'H',
+			NULL);
+	XtAddCallback(view->help, XmNactivateCallback, helpCallback, NULL);
+	XtSetArg(arg[0], XmNmenuHelpWidget, help);
+	XtSetValues(view->menuBar, arg, 1);
+	
+	XtRealizeWidget(view->topLevel);
+	XtAppMainLoop(view->context);
+	
 	return 0;
 }
 #endif
